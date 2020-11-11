299692f4a6debbe5a728cfc9ead621ab3536772a
fredclausen <43556888+fredclausen@users.noreply.github.com>
2020-11-11 09:53:06 -0700

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -9,7 +9,7 @@ PLATFORMS="linux/amd64,linux/arm/v7,linux/arm64"
 
 docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
-docker buildx use homecluster
+#docker buildx use homecluster
 
 # Build & push latest
 docker buildx build --no-cache -t "${REPO}/${IMAGE}:latest" --compress --push --platform "${PLATFORMS}" .
